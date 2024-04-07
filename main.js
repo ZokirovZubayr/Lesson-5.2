5ccc04e290ad609c7fb833b3f90e0c3c8b4cabc6
ZokirovZubayr <zokirovzubayr@gmail.com>
2024-04-07 04:06:28 -0700

    I want to learn git and github so I add this commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let a = prompt("Ismingizni kiriting...");
+let b = prompt("Space idingizni kiriting...");
+let d = prompt("Space passwordingizni kiriting...");
+const name1 = {
+    name: "Zubayr",
+    id: "622949",
+    password: "68927",
+}
+
+console.log(name1);
